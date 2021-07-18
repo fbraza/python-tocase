1e4f31aeeee1f390a4c05b4a87531d9caecfbd6a
fbraza <fbraza@tutanota.com>
2021-07-18 15:39:54 +0200

    test: add test for toCase.snake()

--- tests/test_recase.py
+++ tests/test_recase.py
@@ -44,3 +44,30 @@ def test_camel_should_return_if_only_special_chars():
 
 def test_camel_should_return_empty_string():
     assert toCase("").camel() == ""
+
+
+# tests for camel()
+def test_snake_should_snake_case_usual_strings():
+    assert toCase("snake case").snake() == "snake_case"
+    assert toCase("snakeCase").snake() == "snake_case"
+    assert toCase("SNAKEcase").snake() == "snake_case"
+    assert toCase("Snake_Case").snake() == "snake_case"
+    assert toCase("Snake$%Case").snake() == "snake_case"
+    assert toCase("Snake.Case").snake() == "snake_case"
+    assert toCase("SNAKE_CASE").snake() == "snake_case"
+    assert toCase("__snake_case__").snake() == "snake_case"
+    assert toCase("snake?case").snake() == "snake_case"
+    assert toCase("snake_case").snake() == "snake_case"
+    assert toCase("SNAKECase").snake() == "snakec_ase"
+
+
+def test_snake_should_return_single_letter():
+    assert toCase("s").snake() == "s"
+
+
+def test_snake_should_return_string_if_only_special_chars():
+    assert toCase("£$%^&*()_-+=:;.,'@#~?").snake() == "£$%^&*()_-+=:;.,'@#~?"
+
+
+def test_snake_should_return_empty_string():
+    assert toCase("").snake() == ""
