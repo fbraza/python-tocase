22cef0a023bea3e4574594d38ee3f689a83d194f
fbraza <fbraza@tutanota.com>
2021-07-18 15:41:01 +0200

    refactor: move the logic for generating new string cases into one generic function

--- tocase/tocase.py
+++ tocase/tocase.py
@@ -15,20 +15,35 @@ class toCase:
     def __post_init__(self):
         self.words = regex.findall(self.pattern, self.string)
         self.size = len(self.words)
+        self.separators: dict[str, str] = {
+            "pascal": "",
+            "camel": "",
+            "snake": "_",
+            "title": " ",
+            "kebab": "-",
+            "dot": ".",
+            "header": "-"
+        }
+
+    def __convert(self, kind: str = "pascal"):
+        sep: str = self.separators[kind]
+        if self.size == 0:
+            return self.string
+        elif kind == "pascal" or kind == "title" or kind == "header":
+            return sep.join([w.lower().capitalize() for w in self.words])
+        elif kind == "snake" or kind == "kebab" or "kind" == "dot":
+            return sep.join([w.lower() for w in self.words])
+        elif kind == "camel":
+            return sep.join([w.lower() if i == 0 else w.lower().capitalize() for i, w in enumerate(self.words)]) # noqa: 501
 
     def pascal(self):
-        if self.size == 0:
-            return self.string
-        return "".join([w.lower().capitalize() for w in self.words])
+        return self.__convert("pascal")
 
     def camel(self):
-        if self.size == 0:
-            return self.string
-        return "".join([w.lower() if i == 0 else w.lower().capitalize()
-                        for i, w in enumerate(self.words)])
+        return self.__convert("camel")
 
     def snake(self):
-        pass
+        return self.__convert("snake")
 
     def kebab(self):
         pass
